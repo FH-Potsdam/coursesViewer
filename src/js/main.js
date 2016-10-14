6e6cd6958972d1328019c2c277ba4d460ceff1ec
Q-rec <bela.kurek@berlin.de>
2016-10-14 13:57:39 +0200

    modified main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,14 @@
+var courses = [];
 
-var courses = [
-	{
-		studyCode: "21P-KD",
-		title: "Projekt Kommunikationsdesign: Motion Standard.03: Das Dokumentarische im Animationsfilm",
-		incomWorkspaceId: "6903",
-		firstName: "Prof. Klaus",
-		lastName: "Dufke",
-		beginDate: "20.03.2016",
-		endDate: "30.12.2016",
-		days: "Mittwoch",
-		dayOccurency: "wöchentlich",
-		startTime: "10:00",
-		endTime: "14:00",
-		place: "LW219"
-	},
-	{
-		studyCode: "23P-TD",
-		title: "Identität, Information, Inszenierung: Visuelle Gesten der Beteuerung",
-		incomWorkspaceId: "6953",
-		firstName: "Prof. Constanze",
-		lastName: "Langer",
-		beginDate: "17.03.2016",
-		endDate: "35.12.2016",
-		days: "Dienstag",
-		dayOccurency: "täglich",
-		startTime: "11:00",
-		endTime: "17:00",
-		place: "D103"
-	}
-];
+$.ajax('https://coursesapi.herokuapp.com/courses')
+    .done(function(response) {
+        courses = response.data || response;
+				courses.forEach(displayCourseHtml);
+    })
+    .fail(function(err) {
+        console.log(err);
+    });
+
 function getStudyCode(code) {
 	var studyCode = code;
 	return studyCode;
@@ -46,30 +26,11 @@ function getFullName(first, last) {
 		fullName =  '<p>' + first + " " + last + '</p>';
 	}
 	return fullName;
-};
-function getBeginDate(date) {
-	var beginDate = date;
-	return beginDate;
-};
-function getEndDate(date) {
-	var endDate = date;
-	return endDate;
-};
-function getDays(day) {
-	var days = day;
-	return days;
-};
-function getDayOccurency(occury) {
-	var occurency = occury;
-	return occurency;
-};
-function getTime(start, end) {
-	var fullTime = start;
-	if (end) {
-		fullTime =  '<p>' + start + " - " + end + '</p>';
-	}
-	return fullTime;
-};
+}
+function getCredits(cred){
+	var credits = cred;
+	return credits;
+}
 function getPlace(place) {
 	var fullPlace = place;
 	return fullPlace;
@@ -80,17 +41,11 @@ function displayCourseHtml(course) {
     var htmlStructure =
         '<tr>' +
             '<td>' + getStudyCode(course.studyCode) +'</td>' +
-            '<td>' + getLinkedTitle(course.title, course.incomWorkspaceId) + '</td>' +
+            '<td>' + getLinkedTitle(course.title, course.incomWorkspaceUrl) + '</td>' +
             '<td>' + getFullName(course.firstName, course.lastName) + '</td>' +
-            '<td>' + getBeginDate(course.beginDate) + '</td>' +
-            '<td>' + getEndDate(course.endDate) +'</td>' +
-            '<td>' + getDays(course.days) + '</td>' +
-            '<td>' + getDayOccurency(course.dayOccurency) + '</td>' +
-            '<td>' + getTime(course.startTime, course.endTime) + '</td>' +
-            '<td>' + getPlace(course.place) + '</td>' +
+						'<td>' + getCredits(course.credits) + '</td>'
+            '<td>' + getPlace(course.locations) + '</td>' +
         '</tr>';
     var $elementToInjectIn = $('#results tbody');
     $elementToInjectIn.append(htmlStructure);
 }
-
-courses.forEach(displayCourseHtml);
