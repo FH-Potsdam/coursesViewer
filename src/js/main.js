4387d5d1b73a050b6861412d046568bd2b1cdf11
vogelino <contact@vogelino.com>
2016-10-15 16:56:30 +0200

    Made some cleaning

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,50 @@
 var courses = [];
 
 $.ajax('https://coursesapi.herokuapp.com/courses?limit=1000')
-    .done(function(response) {
-        courses = response.data || response;
-				courses.forEach(displayCourseHtml);
-    })
-    .fail(function(err) {
-        console.log(err);
-    });
+	.done(function(response) {
+		courses = response.data || courses;
+		courses.forEach(displayCourseHtml);
+	})
+	.fail(function(err) {
+		console.error(err);
+	});
 
 function getStudyCode(code) {
 	var studyCode = code;
 	return studyCode;
-};
+}
+
 function getLinkedTitle(title, link) {
 	var courseTitle = title;
 	if (link) {
 		courseTitle = '<a href="' + link + '">' + title  + '</a>';
 	}
 	return courseTitle;
-};
+}
+
 function getFullName(teachersarray) {
 	var teacher = teachersarray.length ? teachersarray[0].fullName : '';
 
 	return teacher;
 }
-function getCredits(cred){
-  if(!cred) return '–';
+
+function getCredits(cred) {
+	if (!cred) return '–';
 	return cred;
 }
-function getDescription(desc){
+
+function getDescription(desc) {
 	return desc;
 }
 
 function displayCourseHtml(course) {
-    var htmlStructure =
-        '<tr>' +
-            '<td>' + getStudyCode(course.studyCode) +'</td>' +
-            '<td>' + getLinkedTitle(course.title, course.incomWorkspaceUrl) + '</td>' +
-            '<td>' + getFullName(course.teachers) + '</td>' +
-						'<td>' + getCredits(course.credits) + '</td>' +
-            '<td>' + getDescription(course.description) + '</td>' +
-        '</tr>';
-    var $elementToInjectIn = $('#results tbody');
-    $elementToInjectIn.append(htmlStructure);
+	$('#results tbody').append(
+		'<tr>' +
+			'<td>' + getStudyCode(course.studyCode) +'</td>' +
+			'<td>' + getLinkedTitle(course.title, course.incomWorkspaceUrl) + '</td>' +
+			'<td>' + getFullName(course.teachers) + '</td>' +
+			'<td>' + getCredits(course.credits) + '</td>' +
+			'<td>' + getDescription(course.description) + '</td>' +
+		'</tr>'
+	);
 }
